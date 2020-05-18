66a582015a4299071c77a6fb8c53a73950dbe4c3
Ricardo <ricardo.a.nava@gmail.com>
2020-05-18 14:02:45 -0700

    Added Header proptypes

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Appbar, useTheme } from 'react-native-paper';
+import { PropTypes } from 'prop-types';
 
 const Header = ({ scene, previous, navigation }) => {
   const theme = useTheme();
+  const { goBack } = navigation;
 
   return (
     <Appbar.Header>
       {previous && (
-        <Appbar.BackAction
-          onPress={navigation.goBack}
-          color={theme.colors.primary}
-        />
+        <Appbar.BackAction onPress={goBack} color={theme.colors.primary} />
       )}
     </Appbar.Header>
   );
+};
+
+Header.propTypes = {
+  navigation: PropTypes.objectOf(PropTypes.func).isRequired
 };
 
 export default Header;
