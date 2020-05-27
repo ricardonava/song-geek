ff631b381e4d0fa2ec07f97daa7181f2b74df6b9
Ricardo <ricardo.a.nava@gmail.com>
2020-05-27 02:34:30 -0700

    more styling with styledcomponents

--- screens/HomeScreen/Login.js
+++ screens/HomeScreen/Login.js
@@ -1,14 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import { Button } from 'react-native-paper';
+import styled from 'styled-components/native';
 import getToken from '../../utilities/getAuthCode';
 
-const styles = StyleSheet.create({
-  button: {
-    alignSelf: 'center'
-  }
-});
+const LoginButton = styled(Button)`
+  align-self: center;
+`;
 
 const authWithSpotify = async (setToken) => {
   const result = await getToken();
@@ -18,13 +16,9 @@ const authWithSpotify = async (setToken) => {
 const Login = ({ setToken }) => {
   return (
     <>
-      <Button
-        style={styles.button}
-        mode="contained"
-        onPress={() => authWithSpotify(setToken)}
-      >
+      <LoginButton mode="contained" onPress={() => authWithSpotify(setToken)}>
         Login
-      </Button>
+      </LoginButton>
     </>
   );
 };
