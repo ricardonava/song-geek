4d1b7211dbd25a4448bc78b213844426d034540e
Ricardo <ricardo.a.nava@gmail.com>
2020-05-28 18:41:25 -0700

    Line Endings

--- screens/HomeScreen/Login.js
+++ screens/HomeScreen/Login.js
@@ -17,6 +17,7 @@ async function authWithSpotify() {
     expires_in: expiresIn
   } = responseJson;
   const authState = { accessToken, refreshToken, expiresIn };
+  console.log(typeof authState.accessToken);
   await SecureStore.setItemAsync('token', JSON.stringify(authState));
 }
 
