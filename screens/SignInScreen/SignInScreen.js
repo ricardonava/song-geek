b6c26e5c7b6708346c2d2102f18d964c4560367b
Ricardo <ricardo.a.nava@gmail.com>
2020-06-06 00:35:53 -0700

    removed useAuthRequest

--- screens/SignInScreen/SignInScreen.js
+++ screens/SignInScreen/SignInScreen.js
@@ -13,26 +13,9 @@ const Container = styled(Surface)`
 const redirect = AuthSession.makeRedirectUri({ useProxy: true });
 
 const SignInScreen = () => {
-  const discovery = {
-    authorizationEndpoint: 'https://accounts.spotify.com/authorize',
-    tokenEndpoint: 'https://accounts.spotify.com/api/token'
-  };
-  const [request, response, promptAsync] = AuthSession.useAuthRequest(
-    {
-      clientId: 'f28d5216d2ff4152abe93aba6c2692ab',
-      clientSecret: 'f6a17d02d6c74c28b617a8b99fe5b8f1',
-      scopes: ['user-library-read'],
-      redirectUri: `${redirect}/`,
-      usePKCE: false
-    },
-    discovery
-  );
-
-  const { signIn } = React.useContext(AuthContext);
-
   return (
     <Container>
-      <Button mode="contained" onPress={() => signIn(promptAsync)}>
+      <Button mode="contained" onPress={() => signIn()}>
         Login
       </Button>
     </Container>
